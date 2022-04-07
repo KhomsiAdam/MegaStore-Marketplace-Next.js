d63f404cc6ae5514b6cd3a885c76a24143c61bf4
FalconHD <youssbak.2015@gmail.com>
2022-04-07 02:10:20 +0000

    merging

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-import type { NextPage } from "next";
-import CategoriesList from "@/components/CategoriesList";
-import NewsSlider from "@/components/NewsSlider";
-import ProductsThumbnails from "@/components/ProductsThumbnails";
-import VerifiedUserSharpIcon from "@mui/icons-material/VerifiedUserSharp";
-import MonetizationOnSharpIcon from "@mui/icons-material/MonetizationOnSharp";
-import SupportAgentSharpIcon from "@mui/icons-material/SupportAgentSharp";
-import { MouseEvent, useContext } from "react";
-import ModalContext from "@/context/ModalContext";
-import FeatherIcon from "feather-icons-react";
-
-=======
 import type { NextPage } from 'next';
 import CategoriesList from '@/components/CategoriesList';
 import NewsSlider from '@/components/NewsSlider';
@@ -21,34 +8,12 @@ import MonetizationOnSharpIcon from '@mui/icons-material/MonetizationOnSharp';
 import SupportAgentSharpIcon from '@mui/icons-material/SupportAgentSharp';
 import { MouseEvent, useContext } from 'react';
 import ModalContext from '@/context/ModalContext';
->>>>>>> a33f3f9f37c1e034982b5e16577a19a836332e2e
 import {
   CategoriesDocument,
   CategoriesQueryVariables,
   ProductsDocument,
   ProductsQueryVariables,
   ProductsQuery,
-<<<<<<< HEAD
-} from "@/graphql/generated/graphql";
-import apolloClient from "@/graphql/apollo";
-import { GetServerSideProps } from "next";
-import { useAppSelector } from "@/redux/hooks";
-import { useRouter } from "next/router";
-import { Fab } from "@mui/material";
-
-const Home: NextPage<CategoriesQueryVariables, ProductsQuery> = ({
-  categories,
-  products,
-}) => {
-  const { toggleModal, setForm } = useContext(ModalContext);
-  const { isLogged, User } = useAppSelector((state) => state.user);
-  const router = useRouter();
-  const handleToggle = (e: MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    if (toggleModal) toggleModal();
-    setForm((e.target as HTMLButtonElement).value);
-  };
-=======
 } from '@/graphql/generated/graphql';
 import apolloClient from '@/graphql/apollo';
 import { GetServerSideProps } from 'next';
@@ -61,51 +26,10 @@ const Home: NextPage<CategoriesQueryVariables, ProductsQuery> = ({
   products,
 }) => {
 
->>>>>>> a33f3f9f37c1e034982b5e16577a19a836332e2e
   return (
     <>
       <div className="h-full xl:h-[520px] overflow-hidden rounded-lg shadow-bm relative flex justify-between flex-col xl:flex-row">
         <CategoriesList mainCategories={categories} />
-<<<<<<< HEAD
-        <div className="w-full xl:w-[75%] flex flex-col justify-between lg:ml-3">
-          <div className="w-full h-[calc(90%-0.75rem)] rounded-lg hidden xl:block">
-            <div className="grid gap-3 grid-rows-[3fr_7fr] w-full h-full rounded-lg">
-              <div className="grid gap-3 grid-cols-[7fr_3fr] h-full rounded-lg">
-                <div className="w-full bg-white rounded-lg"></div>
-                <div className="flex flex-col items-center w-full p-3 bg-white rounded-lg justify-evenly">
-                  {!isLogged ? (
-                    <>
-                      <p className="pb-3 font-bold">Welcome to MegaStore</p>
-                      <div className="flex items-center w-full justify-evenly">
-                        <button
-                          className="btn-primary"
-                          value="register"
-                          onClick={handleToggle}
-                        >
-                          Join
-                        </button>
-                        <button
-                          className="btn-secondary"
-                          value="login"
-                          onClick={handleToggle}
-                        >
-                          Connect
-                        </button>
-                      </div>
-                    </>
-                  ) : (
-                    <>
-                      GO TO MY STORE
-                      <Fab
-                        onClick={() => router.push("/vendor")}
-                        color="primary"
-                        aria-label="add"
-                      >
-                        <FeatherIcon icon="home" width="20" height="20" />
-                      </Fab>
-                    </>
-                  )}
-=======
         <div className='w-full xl:w-[75%] flex flex-col justify-between lg:ml-3'>
           <div className='w-full h-[calc(90%-0.75rem)] rounded-lg hidden xl:block'>
             <div className='grid gap-3 grid-rows-[3fr_7fr] w-full h-full rounded-lg'>
@@ -116,7 +40,6 @@ const Home: NextPage<CategoriesQueryVariables, ProductsQuery> = ({
                   <div className='flex items-center justify-between w-full'>
                     <AuthComponent />
                   </div>
->>>>>>> a33f3f9f37c1e034982b5e16577a19a836332e2e
                 </div>
               </div>
               <div className="w-full h-full overflow-hidden bg-white rounded-lg">
@@ -124,30 +47,10 @@ const Home: NextPage<CategoriesQueryVariables, ProductsQuery> = ({
               </div>
             </div>
           </div>
-<<<<<<< HEAD
-          <div className="flex flex-col items-center w-full p-3 bg-white rounded-lg justify-evenly xl:hidden">
-            <p className="pb-3 font-bold">Welcome to MegaStore</p>
-            <div className="flex items-center w-full justify-evenly">
-              <button
-                className="btn-primary"
-                value="register"
-                onClick={handleToggle}
-              >
-                Join
-              </button>
-              <button
-                className="btn-secondary"
-                value="login"
-                onClick={handleToggle}
-              >
-                Connect
-              </button>
-=======
           <div className='flex flex-col items-center w-full p-3 bg-white rounded-lg justify-evenly xl:hidden'>
             <p className='pb-3 font-bold'>Welcome to MegaStore</p>
             <div className='flex items-center w-full justify-evenly'>
               <AuthComponent />
->>>>>>> a33f3f9f37c1e034982b5e16577a19a836332e2e
             </div>
           </div>
           <div className="bg-white w-full my-3 xl:my-0 py-3 xl:h-[10%] rounded-lg flex justify-between items-center px-1 lg:px-3">
